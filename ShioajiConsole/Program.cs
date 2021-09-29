2ceb913a7d053a934b307206dd6a40b5d8486855
WhoIWannaBe <atheismy1998@gmail.com>
2021-09-30 02:50:36 +0800

    no message

--- ShioajiConsole/Program.cs
+++ ShioajiConsole/Program.cs
@@ -2,6 +2,9 @@
 using Sinopac.Shioaji;
 using System.Collections.Generic;
 using System.Configuration;
+using Microsoft.Data.Analysis;
+using System.Text;
+
 
 namespace ShioajiConsole
 {
@@ -12,6 +15,7 @@ namespace ShioajiConsole
         {
             MyApi ma = new MyApi();
             ma.Login();
+            Console.WriteLine("Login Success. Waiting For Command");
 
             string command = "";
 
@@ -30,21 +34,29 @@ namespace ShioajiConsole
                     break;
                 }
 
+                // 下指令 !command
                 switch (commands[0].Substring(1))
                 {
+                    // 取得歷史資料
                     case "history":
-                        if(commands.Length >= 2)
+                        if(commands.Length > 2)
                         {
-                            Console.WriteLine(ma.getHistoricalMarketData(commands.Length > 2 ? commands[1] : "2330", commands[1]));
+                            //Console.WriteLine(ma.getHistoricalMarketData(commands[1], commands[2]));
+                            ma.getHistoricalMarketData(commands[1], commands[2]);
                         }
                         else
                         {
-                            Console.WriteLine(ma.getHistoricalMarketData(commands.Length > 2 ? commands[1] : "2330", DateTime.Now.ToString("yyyy-MM-dd")));
+                            //Console.WriteLine(ma.getHistoricalMarketData(commands[1], DateTime.Now.ToString("yyyy-MM-dd")));
+                            ma.getHistoricalMarketData(commands[1], DateTime.Now.ToString("yyyy-MM-dd"));
                         }
                         break;
+                    // 取得即時資訊
                     case "streaming":
-                        ma.getStreaming(commands.Length > 2 ? commands[1] : "2330");
-                        System.Threading.Thread.Sleep(5000);
+                        if(commands.Length >= 2)
+                        {
+                            ma.getStreaming(commands[1]);
+                            System.Threading.Thread.Sleep(5000);
+                        }                                               
                         break;
                     default:
                         break;
@@ -67,6 +79,25 @@ namespace ShioajiConsole
         {
             var contract = _api.Contracts.Stocks[type][stockCode];
             Ticks ticks = _api.Ticks(contract, date);
+            StringBuilder Content = new StringBuilder();
+            Content.AppendLine("ts\t\t\t\tvolume\tclose\tbid_price\tbid_volume\task_price\task_volume");
+            for(var i = 0; i < ticks.volume.Count; ++i)
+            {
+                Content.AppendLine(ticks.ts[i] + "\t\t\t\t" + ticks.volume[i] + "\t" + ticks.close[i] + "\t" + ticks.bid_price[i] + "\t" + ticks.bid_volume[i] + "\t" + ticks.ask_price[i] + "\t" + ticks.ask_volume[i]);
+            }
+           
+            /*
+            var volume = new PrimitiveDataFrameColumn<long>("volume", ticks.volume);
+            var close = new PrimitiveDataFrameColumn<double>("close", ticks.close);
+            var ts = new PrimitiveDataFrameColumn<long>("ts", ticks.ts);
+            var bid_price = new PrimitiveDataFrameColumn<double>("bid_price", ticks.bid_price);
+            var bid_volume = new PrimitiveDataFrameColumn<long>("bid_volume", ticks.bid_volume);
+            var ask_price = new PrimitiveDataFrameColumn<double>("ask_price", ticks.ask_price);
+            var ask_volume = new PrimitiveDataFrameColumn<long>("ask_volume", ticks.ask_volume);
+            var df_ticks = new DataFrame(ts, volume, close, bid_price, bid_volume, ask_price, ask_volume);
+            Console.WriteLine(df_ticks);
+            */
+
             return ticks;
         }
 
